0a4af0669da38614ec30d2c7a656d03f4231fb18
Abhishek Jha <abhi.jha.cs@gmail.com>
2024-03-10 14:13:54 +0530

    Added loading state in button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,23 +1,39 @@
 "use client";
 
+import { RedoOutlined } from "@ant-design/icons";
 import styles from "./button.module.css";
+import { ReactNode } from "react";
 
-type ButtonProps = {
-    text: string;
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    text: ReactNode;
     onClick: () => void;
     bgColor?: string;
     textColor?: string;
     size?: "sm" | "md" | "lg";
+    isLoading?: boolean;
+    loadingText?: string;
+    icon?: ReactNode;
 };
 
-export default function Button({ text, bgColor, textColor, onClick, size = "md" }: ButtonProps) {
+export default function Button({
+    text,
+    bgColor,
+    textColor,
+    onClick,
+    size = "md",
+    isLoading,
+    loadingText,
+    icon,
+}: ButtonProps) {
     return (
         <button
-            className={`${styles.custom__btn} ${bgColor ? "" : "default__bg"} ${styles[`${size}__btn`]}`}
+            className={`${styles.custom__btn} ${bgColor ? "" : "default__bg"} ${styles[`${size}__btn`]} ${
+                isLoading ? styles.loading : ""
+            }`}
             onClick={onClick}
             style={{ backgroundColor: bgColor ?? "", color: textColor ?? "" }}
         >
-            {text}
+            {isLoading ? <RedoOutlined className={styles.spinner} /> : null} {isLoading ? loadingText : text} {icon}
         </button>
     );
 }
