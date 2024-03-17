0692be49b3306f8c0283942c55d06a2f88a54bae
Abhishek Jha <abhi.jha.cs@gmail.com>
2024-03-17 11:55:12 +0530

    getting ready page UI improvements

--- src/app/getting-ready/page.tsx
+++ src/app/getting-ready/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { useEffect } from "react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import { Flex, Typography } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+const { Title } = Typography;
 
+import styles from "./gettingready.module.css";
 import { authServices } from "@/services/auth";
 
 export default function GettingReady() {
@@ -17,14 +22,23 @@ export default function GettingReady() {
             if (isLoginSuccessFul) {
                 router.push("/dashboard/project");
             } else {
-                console.log("error");
+                router.push("/error");
             }
         })();
     }, [sessionDetails]);
 
     return (
-        <main>
-            <h1>Getting ready</h1>
-        </main>
+        <Flex vertical justify="center" align="center" className={styles.container}>
+            <Flex gap="16px">
+                <Title level={1} className={styles.heading}>
+                    Getting ready
+                </Title>
+                <LoadingOutlined style={{ fontSize: "32px" }} />
+            </Flex>
+            <Title level={3} className={styles.subheading}>
+                Tighten your seatbelt, we are getting ready to take off 🚀
+            </Title>
+            <Image src="/assets/RocketLaunch.svg" alt="getting ready" width={260} height={260} />
+        </Flex>
     );
 }
