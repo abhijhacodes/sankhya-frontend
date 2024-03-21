7710c778741985ace34e904053aae9b2a66ad151
Abhishek Jha <abhi.jha.cs@gmail.com>
2024-03-21 20:50:55 +0530

    return set cookie header from api instead of setting directly

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,4 +1,3 @@
-import { cookies } from "next/headers";
 import DefaultAxiosInstance from "@/services/clients/axios";
 
 export async function POST(req: Request) {
@@ -8,9 +7,17 @@ export async function POST(req: Request) {
             const loginResponse = await DefaultAxiosInstance.post("/auth/login", {
                 email,
             });
-            const token = loginResponse.headers["set-cookie"]?.[0]?.slice(6) as string | undefined;
-            cookies().set("token", token!);
-            return Response.json({ message: "Login successful" }, { status: 200 });
+            const setCookieHeader = loginResponse.headers["set-cookie"]?.[0] as string | undefined;
+
+            return Response.json(
+                { message: "Login successful" },
+                {
+                    status: 200,
+                    headers: {
+                        "Set-Cookie": `${setCookieHeader}`,
+                    },
+                }
+            );
         } catch (error: any) {
             return Response.json({ message: "Internal server error" }, { status: 500 });
         }
