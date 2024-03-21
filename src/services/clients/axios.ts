b173f14683b08cdd1e8195df5355145882b2ffd8
Abhishek Jha <abhi.jha.cs@gmail.com>
2024-03-21 22:15:20 +0530

    remove headers

--- src/services/clients/axios.ts
+++ src/services/clients/axios.ts
@@ -3,10 +3,6 @@ import axios from "axios";
 const DefaultAxiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_SANKHYA_BACKEND_URL,
     withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Credentials": true,
-    },
 });
 
 export default DefaultAxiosInstance;
