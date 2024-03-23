0cc9641bc12df89c83a23cb82304011cfcd09660
Abhishek Jha <abhi.jha.cs@gmail.com>
2024-03-23 18:31:57 +0530

    Added navigation on Sankhya header click

--- src/components/Navbar/Header.tsx
+++ src/components/Navbar/Header.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import styles from "./navbar.module.css";
 
 export default function HeroHeader() {
+    const router = useRouter();
+    const onLogoClick = () => {
+        router.push("/");
+    };
+
     return (
         <nav className={styles.header}>
-            <div className={styles.navbar__title__container}>
+            <div className={styles.navbar__title__container} onClick={onLogoClick}>
                 <h3 className={styles.navbar__title}>Sankhya Pro</h3>
                 <h5 className={styles.navbar__subtitle}>संख्या</h5>
             </div>
